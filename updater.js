d584d2020954c3e4c496a205ceda693853a4bca9
milankomaj <milankomaj+Github@gmail.com>
2024-08-04 23:06:11 +0200

    Update updater.js
    removeDeprecatedAttrs

--- updater.js
+++ updater.js
@@ -40,7 +40,7 @@ user.fetchContent()
                     'reusePaths',
                     'convertTransform',
                     'removeEmptyContainers',
-                    'removeDeprecatedAttributes',
+                    'removeDeprecatedAttrs',
                     'convertTransform',
                     'sortAttrs'
                 ]
@@ -72,7 +72,7 @@ user.fetchContent()
                     'reusePaths',
                     'convertTransform',
                     'removeEmptyContainers',
-                    'removeDeprecatedAttributes',
+                    'removeDeprecatedAttrs',
                     'convertTransform',
                     'sortAttrs'
                 ]
